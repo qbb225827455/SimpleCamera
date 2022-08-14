1f1a2730e189b9178613705a3a479e77f20b1c09
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-08-14 23:07:18 +0800

    新增 錄影功能

--- SimpleCamera/CameraViewController.swift
+++ SimpleCamera/CameraViewController.swift
@@ -7,29 +7,56 @@
 
 import UIKit
 import AVFoundation
+import AVKit
 
 class CameraViewController: UIViewController {
     
     let captureSession = AVCaptureSession()
     
+    // MARK: -
+    
     var rearCamera: AVCaptureDevice?
     var frontCamera: AVCaptureDevice?
     var currentDevice: AVCaptureDevice!
     
+    // MARK: -
+    
     var imageOutput: AVCapturePhotoOutput!
     var captureImage: UIImage?
     
+    // MARK: -
+    
+    var videoOutput: AVCaptureMovieFileOutput!
+    var isRecording = false
+    
+    // MARK: -
+    
     var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    // MARK: - Timer
+    
+    var timer: Timer = Timer()
+    var timeCount: Int = 0
+    var timerIsCounting: Bool = false
+    
+    // MARK: - UISwipeGestureRecognizer
     
     var toggleCameraSwipeUp = UISwipeGestureRecognizer()
     var toggleCameraSwipeDown = UISwipeGestureRecognizer()
-    
+    var swipeLeft = UISwipeGestureRecognizer()
     var pinchGesture = UIPinchGestureRecognizer()
-
-    @IBOutlet var cameraBtn: UIButton!
-    @IBOutlet var scaleLabel: UILabel!
     
-    @IBAction func capture(sender: UIButton) {
+    // MARK: - @Outlet
+
+    @IBOutlet var photoBtn: UIButton!
+    @IBOutlet var videoBtn: UIButton!
+    
+    @IBOutlet var scaleLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+    // MARK: - 拍照
+    
+    @IBAction func capturePhoto(sender: UIButton) {
         
         let photoSetting = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
         photoSetting.isAutoStillImageStabilizationEnabled = true
@@ -44,9 +71,45 @@ class CameraViewController: UIViewController {
         imageOutput.capturePhoto(with: photoSetting, delegate: self)
     }
     
+    // MARK: - 錄影
+    
+    @IBAction func captureVideo(sender: UIButton) {
+        
+        isRecording.toggle()
+        if isRecording {
+            
+            timeCount = 0
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nibName, repeats: true)
+            
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.repeat, .autoreverse, .allowUserInteraction], animations: { () -> Void in
+                
+                self.videoBtn.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: nil)
+            
+            let outputPath = NSTemporaryDirectory() + "output.mov"
+            let outputFileURL = URL(fileURLWithPath: outputPath)
+            videoOutput.startRecording(to: outputFileURL, recordingDelegate: self)
+            
+        } else {
+            
+            timer.invalidate()
+            
+            UIView.animate(withDuration: 0.5, delay: 1.0, options: [], animations: { () -> Void in
+                self.videoBtn.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: nil)
+            timeLabel.text = "  00 : 00 : 00  "
+            videoBtn.layer.removeAllAnimations()
+            videoOutput.stopRecording()
+        }
+    }
+    
+    // MARK: - 關閉相片預覽頁面
+    
     @IBAction func closePhotoView(segue: UIStoryboardSegue) {
         dismiss(animated: true)
     }
+    
+    // MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,7 +117,16 @@ class CameraViewController: UIViewController {
         let largeConfig = UIImage.SymbolConfiguration(pointSize: 50)
         let largeBoldDoc = UIImage(systemName: "circle.inset.filled", withConfiguration: largeConfig)
         
-        cameraBtn.setImage(largeBoldDoc, for: .normal)
+        photoBtn.setImage(largeBoldDoc, for: .normal)
+        
+        let largeBoldDoc2 = UIImage(systemName: "viewfinder.circle.fill", withConfiguration: largeConfig)
+        
+        videoBtn.setImage(largeBoldDoc2, for: .normal)
+        videoBtn.isHidden = true
+        
+        timeLabel.isHidden = true
+        timeLabel.layer.masksToBounds = true
+        timeLabel.layer.cornerRadius = 10
         
         scaleLabel.layer.masksToBounds = true
         scaleLabel.layer.cornerRadius = 15
@@ -66,6 +138,8 @@ class CameraViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - 判斷裝置有無旋轉，更新預覽畫面
     
     override func viewWillLayoutSubviews() {
         
@@ -105,6 +179,8 @@ class CameraViewController: UIViewController {
         cameraPreviewLayer!.frame = view.layer.bounds
     }
     
+    // MARK: - captureSession 初始化
+    
     func configure() {
         
         captureSession.sessionPreset = AVCaptureSession.Preset.photo
@@ -125,9 +201,11 @@ class CameraViewController: UIViewController {
         }
         
         imageOutput = AVCapturePhotoOutput()
+        videoOutput = AVCaptureMovieFileOutput()
         
         captureSession.addInput(captureDeviceInput)
         captureSession.addOutput(imageOutput)
+        
         
         // 建立相機預覽
         cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
@@ -135,8 +213,10 @@ class CameraViewController: UIViewController {
         cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
         cameraPreviewLayer?.frame = view.layer.frame
 
-        view.bringSubviewToFront(cameraBtn)
+        view.bringSubviewToFront(photoBtn)
+        view.bringSubviewToFront(videoBtn)
         view.bringSubviewToFront(scaleLabel)
+        view.bringSubviewToFront(timeLabel)
         captureSession.startRunning()
         
         // 切換相機
@@ -149,7 +229,13 @@ class CameraViewController: UIViewController {
         
         pinchGesture.addTarget(self, action: #selector(pinchToZoom(_:)))
         view.addGestureRecognizer(pinchGesture)
+        
+        swipeLeft.direction = .left
+        swipeLeft.addTarget(self, action: #selector(changeBtn))
+        view.addGestureRecognizer(swipeLeft)
     }
+    
+    // MARK: - 更換使用的相機(前置 or 後置)
     
     @objc func toggleCamera() {
         
@@ -179,6 +265,8 @@ class CameraViewController: UIViewController {
         currentDevice = newCurrentDevice
         captureSession.commitConfiguration()
     }
+    
+    // MARK: - 縮放手勢
     
     @objc func pinchToZoom(_ gesture: UIPinchGestureRecognizer) {
         
@@ -216,13 +304,80 @@ class CameraViewController: UIViewController {
         }
     }
     
+    // MARK: - SwipeLeft 更換相機模式(拍照 or 錄影)
+    
+    @objc func changeBtn() {
+        photoBtn.isHidden.toggle()
+        videoBtn.isHidden.toggle()
+        timeLabel.isHidden.toggle()
+        
+        if !photoBtn.isHidden {
+            for output in captureSession.outputs {
+                captureSession.removeOutput(output)
+            }
+            if captureSession.canAddOutput(imageOutput) {
+                captureSession.addOutput(imageOutput)
+            }
+        }
+        if !videoBtn.isHidden {
+            for output in captureSession.outputs {
+                captureSession.removeOutput(output)
+            }
+            if captureSession.canAddOutput(videoOutput) {
+                captureSession.addOutput(videoOutput)
+            }
+        }
+    }
+    
+    // MARK: - timeLabel 的時間顯示
+    
+    @objc func timerCounter() {
+        
+        timeCount += 1
+        let time = timeCountToHoursMinutesSeconds(timeCount: timeCount)
+        let timeString = timeCountToString(hours: time.0, minutes: time.1, seconds: time.2)
+        timeLabel.text = timeString
+    }
+    
+    func timeCountToHoursMinutesSeconds(timeCount : Int) -> (Int, Int, Int) {
+        
+        return ((timeCount / 3600), ((timeCount % 3600) / 60), ((timeCount % 3600) % 60))
+    }
+    
+    func timeCountToString(hours: Int, minutes: Int, seconds: Int) -> String {
+        
+        var timeString = "  "
+        timeString += String(format: "%02d", hours)
+        timeString += " : "
+        timeString += String(format: "%02d", minutes)
+        timeString += " : "
+        timeString += String(format: "%02d", seconds)
+        timeString += "  "
+        
+        return timeString
+    }
+    
+    
+    // MARK: - Navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showImage" {
+        switch segue.identifier {
+        case "showImage":
             let destinationVC = segue.destination as! PhotoViewController
             destinationVC.image = captureImage
+            
+        case "playVideo":
+            let destinationVC = segue.destination as! AVPlayerViewController
+            let videoFileURL = sender as! URL
+            destinationVC.player = AVPlayer(url: videoFileURL)
+            
+        default:
+            return
         }
     }
 }
+
+// MARK: - AVCapturePhotoCaptureDelegate
 
 extension CameraViewController: AVCapturePhotoCaptureDelegate {
     
@@ -238,5 +393,19 @@ extension CameraViewController: AVCapturePhotoCaptureDelegate {
         
         captureImage = UIImage(data: imageData)
         performSegue(withIdentifier: "showImage", sender: self)
+    }
+}
+
+// MARK: - AVCaptureFileOutputRecordingDelegate
+
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        
+        guard error == nil else {
+            return
+        }
+        
+        performSegue(withIdentifier: "playVideo", sender: outputFileURL)
     }
 }
